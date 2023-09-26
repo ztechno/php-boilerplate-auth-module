1f15ddda02dabd4237839c0715a7187dac077653
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-09-26 21:37:12 +0700

    first commit

---
+++ guards/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$auth = auth();
+$publicRoutes = [
+    'auth/login'
+];
+
+if(empty($auth) && !in_array($route, $publicRoutes))
+{
+    header("location: ".routeTo('auth/login'));
+    die();
+}
+
+if($auth && $route == 'auth/login')
+{
+    header("location: ".env('AUTH_AFTER_LOGIN_SUCCESS','/'));
+    die();
+}
---
+++ process/login.php
@@ -0,0 +1,42 @@
+<?php
+
+use Core\Request;
+use Core\Session;
+use Core\Database;
+
+$success_msg = get_flash_msg('success');
+$error_msg = get_flash_msg('error');
+$old = get_flash_msg('old');
+
+if(Request::isMethod('post'))
+{
+    // login process here
+    $db = new Database;
+    $user = $db->single('users', [
+        'username' => $_POST['username'],
+        'password' => md5($_POST['password'])
+    ]);
+
+    if($user)
+    {
+        Session::set(['user_id'=>$user->id]);
+        header('location:'.routeTo(env('AUTH_AFTER_LOGIN_SUCCESS', '/')));
+        die();
+    }
+    else
+    {
+        set_flash_msg([
+            'error'=>__('auth.message.login_fail'),
+            'old'  => $_POST
+        ]);
+        header('location:'.routeTo('auth/login'));
+        die();
+    }
+
+}
+
+return view('auth/views/login', compact(
+    'success_msg',
+    'error_msg',
+    'old'
+));
